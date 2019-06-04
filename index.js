4000f60b9eade161482908b8fb70084f483361df
Florian <info@ftws.de>
2019-06-04 12:36:05 +0200

    fix error

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const https = require('https');
 const js2xmlparser = require('js2xmlparser');
 const xml2jsPareseString = require('xml2js').parseString;
 
-const EbayApiRequest = require('./lib/ebay-api-request');
 const EbayApiResponse = require('./lib/ebay-api-response');
 
 const Queue = require('promise-queue');
@@ -281,6 +280,113 @@ function getQueue() {
 
     return queue;
 }
+
+//TODO: May be move to separate file -> currently not possible: class use: defaultCompatLevel + defaultSiteId
+
+/**
+ *
+ */
+class EbayApiRequest {
+
+    /**
+     *
+     * @param apiCallName
+     * @param compatLevel
+     * @param siteId
+     * @param authToken
+     * @param body
+     * @param retryCount
+     */
+    constructor(apiCallName, {compatLevel = null, siteId = null, authToken = '', body = '', retryCount = 2} = {}) {
+        this.data = {};
+        this.data.apiCallName = apiCallName;
+        this.data.compatLevel = compatLevel ? compatLevel : defaultCompatLevel;
+        this.data.siteId = siteId ? siteId : defaultSiteId;
+        this.data.authToken = authToken;
+        this.data.body = body;
+        this.retryCount = retryCount;
+    }
+
+    /**
+     *
+     * @param value
+     * @returns {*|this}
+     */
+    apiCallName(value = false) {
+        if (value) {
+            this.data.apiCallName = value;
+            return this;
+        }
+        return this.data.apiCallName;
+    }
+
+    /**
+     *
+     * @param value
+     * @returns {*|this}
+     */
+    compatLevel(value = false) {
+        if (value) {
+            this.data.compatLevel = value;
+            return this;
+        }
+        return this.data.compatLevel;
+    }
+
+    /**
+     *
+     * @param value
+     * @returns {*|this}
+     */
+    siteId(value = false) {
+        if (value) {
+            this.data.siteId = value;
+            return this;
+        }
+        return this.data.siteId;
+    }
+
+    /**
+     *
+     * @param value
+     * @returns {*|this}
+     */
+    authToken(value = false) {
+        if (value) {
+            this.data.authToken = value;
+            return this;
+        }
+        return this.data.authToken;
+    }
+
+    /**
+     *
+     * @param value
+     * @returns {*|this}
+     */
+    body(value = false) {
+        if (value) {
+            this.data.body = value;
+            return this;
+        }
+        return this.data.body;
+    }
+
+    markRequestSend() {
+        this.retryCount -= 1;
+    }
+
+    retryAllowed() {
+        return this.retryCount > 0;
+    }
+
+    // TODO: at debug output remove token from String
+    /*toString() {
+
+    }*/
+}
+
+
 
 
 module.exports.initQueue = initQueue;
